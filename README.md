e5fc608a52b323c77930cf6aa73716e550db1cd7
Floyd Everest <floyd.everest@monash.edu>
2024-06-02 12:06:17 +1000

    Implemented SPRT with LogLikelihood object argument on construction.

--- README.md
+++ README.md
@@ -18,13 +18,20 @@ We can construct the corresponding SPRT as follows:
 
 ```python
 import numpy as np
-from test_smart.sprt import SPRT, exponential_llr
+from test_smart.sprt import SPRT, ExponentialLogLikelihood, NormalLogLikelihood
 exp_sprt = SPRT(
   alpha = 0.05,
   beta = 0.05,
   theta0 = 1,
   theta1 = 2,
-  llr = exponential_llr()
+  loglikelihood = ExponentialLogLikelihood()
+)
+norm_sprt = SPRT(
+  alpha = 0.05,
+  beta = 0.05,
+  theta0 = 1,
+  theta1 = 2,
+  loglikelihood = NormalLogLikelihood(sigma = 1)
 )
 ```
 
@@ -39,6 +46,17 @@ exp_sprt.test(np.array([2.00, 3.26, 2.46, 2.12, 4.88]))
 exp_sprt.summary()
 # {'null': 'theta = 1',
 #  'alternative': 'theta = 2',
+#  'loglikelihood': <Exponential(theta) log-Likelihood>,
+#  'decision': 'Reject the null hypothesis',
+#  'N': 6}
+norm_sprt.test(1.5)
+# <Decision.CONTINUE: 'Continue testing'>
+norm_sprt.test(np.array([2.00, 3.26, 2.46, 2.12, 4.88]))
+# <Decision.REJECT: 'Reject the null hypothesis'>
+norm_sprt.summary()
+# {'null': 'theta = 1',
+#  'alternative': 'theta = 2',
+#  'loglikelihood': <Normal(theta, 1)) log-Likelihood>,
 #  'decision': 'Reject the null hypothesis',
 #  'N': 6}
  ```
--- test_smart/sprt.py
+++ test_smart/sprt.py
@@ -3,11 +3,63 @@ This file implements Wald's Sequential Probability Ratio Test using the
 `SeqHypothesisTest` framework defined in hypothesis.py
 """
 
+from abc import ABC, abstractmethod
+
 from test_smart.hypothesis import SeqHypothesisTest, Decision
 
-from typing import Callable
-
 import numpy as np
+
+
+class LogLikelihood(ABC):
+    """
+    A base class for implementing log-likelihood functions for use by SPRT.
+    """
+
+    @abstractmethod
+    def ll(self, x: np.ndarray, theta: np.floating):
+        """
+        Log-likelihood ratio for the simple-vs-simple hypothesis test defined
+        by theta0 and theta1.
+        """
+        pass
+
+    @abstractmethod
+    def __repr__(self):
+        """
+        Defines the representation of the Likelihood function for display
+        in summaries.
+        """
+        pass
+
+
+class ExponentialLogLikelihood(LogLikelihood):
+    """
+    Implements the likelihood function for exponential distributions.
+    """
+
+    def ll(self, x: np.ndarray, theta: np.floating):
+        return np.log(theta) - x * theta
+
+    def __repr__(self):
+        return "<Exponential(theta) log-Likelihood>"
+
+
+class NormalLogLikelihood(LogLikelihood):
+    """
+    Implements the log-likelihood function for normal distributions relative to a fixed
+    scale parameter sigma.
+    """
+
+    def __init__(self, sigma: np.floating = 1):
+        self.sigma = sigma
+
+    def ll(self, x: np.ndarray, theta: np.floating):
+        # fmt: off
+        return (x - theta) ** 2 / (2 * self.sigma**2)
+        # fmt: on
+
+    def __repr__(self):
+        return f"<Normal(theta, {self.sigma})) log-Likelihood>"
 
 
 class SPRT(SeqHypothesisTest):
@@ -17,7 +69,7 @@ class SPRT(SeqHypothesisTest):
     for the mean theta of a population with given distribution.
 
     Currently, SPRT is implemented for testing simple-versus-simple hypotheses
-    for the Exponential Normal location parameters. When testing the Normal
+    for the Exponential scale and Normal location parameters. When testing the Normal
     mean, you may optionally specify the standard deviation `sigma` during
     construction.  If left unspecified, it is assumed to be 1.
     """
@@ -28,7 +80,7 @@ class SPRT(SeqHypothesisTest):
         beta: np.floating,
         theta0: np.floating,
         theta1: np.floating,
-        llr: Callable[[np.floating, np.floating, np.ndarray], np.ndarray],
+        loglikelihood: LogLikelihood,
         **kwargs,
     ):
         super().__init__(alpha)
@@ -38,12 +90,17 @@ class SPRT(SeqHypothesisTest):
         self.a = np.log(beta / (1 - alpha))
         self.b = np.log((1 - alpha) / beta)
         self.S = np.array([0])
-        self.llr = llr(theta0, theta1)
+        self.loglikelihood = loglikelihood
 
     def test(self, x: np.ndarray) -> Decision:
         self.observations = np.append(self.observations, x)
         self.S = np.append(
-            self.S, np.cumsum(self.llr(x)) + self.S[-1]
+            self.S,
+            np.cumsum(
+                self.loglikelihood.ll(x, self.theta0)
+                - self.loglikelihood.ll(x, self.theta1)
+            )
+            + self.S[-1],
         )
         if self.S[-1] <= self.a:
             self.decision = Decision.ACCEPT
@@ -57,21 +114,7 @@ class SPRT(SeqHypothesisTest):
         return {
             "null": f"theta = {self.theta0}",
             "alternative": f"theta = {self.theta1}",
+            "loglikelihood": self.loglikelihood,
             "decision": self.decision.value,
             "N": len(self.observations),
         }
-
-
-def exponential_llr() -> Callable[[np.floating, np.floating], np.ndarray]:
-    return lambda theta0, theta1: np.vectorize(
-        lambda xi: -np.log(theta1 / theta0) + xi * (theta1 - theta0) / (theta0 * theta1)
-    )
-
-
-def normal_llr(
-    sigma: np.floating = 1,
-) -> Callable[[np.floating, np.floating, np.ndarray], np.ndarray]:
-    return lambda theta0, theta1: np.vectorize(
-        lambda xi: (xi - theta0) ** 2 / (2 * sigma**2)
-        - (xi - theta1) ** 2 / (2 * sigma**2)
-    )
