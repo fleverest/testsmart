396f7ce2d16bc9dbe661f39a6127fda0f0b4aa40
Floyd Everest <floyd.everest@monash.edu>
2024-06-26 02:00:21 +1000

    add extra summaries for finite populations

--- test_smart/utils.py
+++ test_smart/utils.py
@@ -1,43 +1,71 @@
 import numpy as np
 
 
+class TooManySamplesError(Exception):
+    """
+    An error raised when the number of samples observed exceeds the prespecified
+    population size.
+    """
+
+    def __init__(self, new_size, old_size, population_size):
+        super().__init__(
+            f"Exceeded population size: cannot add further observations. Tried to add"
+            f" {new_size} observations when {old_size}/{population_size} observations"
+            f" had already been taken."
+        )
+
+
 class RunningSummaries:
     """
     A class for calculating some summaries of a stream of data:
-      1. Count
-      2. Cumulative sum
-      3. Running mean (Welford's algorithm)
-      4. Running (population) variance (Welford's algorithm)
+      1. Count,
+      2. cumulative sum,
+      3. running mean (Welford's algorithm),
+      4. running (population) variance (Welford's algorithm),
     These summaries are useful for a variety of inferential tasks.
     """
 
     def __init__(self):
         self._data = []
         self._count = 0
-        self._cumsums = []
+        self._sums = []
         self._means = []
         self._vars = []
 
     @property
     def hist_sums(self) -> np.ndarray:
         """
-        The historical sums of the data stream, up to the current time.
+        The historical sums of the data stream, up to the current time point.
         """
-        return np.array(self._cumsums[:-1])
+        return np.array(self._sums)
+
+    @property
+    def prev_sum(self) -> float:
+        """
+        The previous sum of the data stream, i.e. at the second-to-last time point.
+        """
+        return self._sums[-2] if len(self._sums) > 1 else 0
 
     @property
     def sum(self) -> float:
         """
         The current total sum of the data stream.
         """
-        return self._cumsums[-1] if self._cumsums else 0
+        return self._sums[-1] if self._sums else 0
 
     @property
     def hist_means(self) -> np.ndarray:
         """
         The historical running mean of the data stream, up to the current time.
         """
-        return np.array(self._means[:-1])
+        return np.array(self._means)
+
+    @property
+    def prev_mean(self) -> float:
+        """
+        The previous mean of the data stream, i.e. at the second-to-last time point.
+        """
+        return self._means[-2] if len(self._means) > 1 else np.nan
 
     @property
     def mean(self) -> float:
@@ -52,7 +80,14 @@ class RunningSummaries:
         The historical running (population) variance of the data stream, up to the
         current time.
         """
-        return np.array(self._vars[:-1])
+        return np.array(self._vars)
+
+    @property
+    def prev_var(self) -> float:
+        """
+        The previous variance of the data stream, i.e. at the second-to-last time point.
+        """
+        return self._vars[-2] if len(self._vars) > 1 else np.nan
 
     @property
     def var(self) -> float:
@@ -69,11 +104,13 @@ class RunningSummaries:
         return self._count
 
     def add(self, x: np.ndarray) -> None:
+        if not isinstance(x, np.ndarray):
+            x = np.array(x, ndmin=1)
         if self.count == 0:
             # Starting values
             self._data.append(x[0])
             self._count = 1
-            self._cumsums.append(x[0])
+            self._sums.append(x[0])
             self._means.append(x[0])
             self._vars.append(0)
             x = x[1:]
@@ -83,11 +120,104 @@ class RunningSummaries:
             # Increment counter
             self._count = self._count + 1
             # Add to cumulative sum
-            self._cumsums.append(self.sum + xn)
+            self._sums.append(self.sum + xn)
             # Welford's online running mean and (population) variance
             self._means.append(self.sum / self.count)
             self._vars.append(
                 self.var
                 + ((xn - self._means[-2]) * (xn - self._means[-1]) - self.var)
                 / self.count
+            )
+
+
+class FPRunningSummaries(RunningSummaries):
+    """
+    Just like RunningSummaries but with extras for finite-populations:
+    Currently, also calculates out-of-sample means, given an initial reference mean.
+    """
+
+    def __init__(self, pop_size: int, pop_mean: float):
+        super().__init__()
+        self._pop_size = pop_size
+        self._pop_mean = pop_mean
+        self._oos_means = [self.pop_mean]
+        self._oos_sums = [self.pop_mean * self.pop_size]
+
+    @property
+    def pop_mean(self):
+        """
+        The mean of the full population.
+        """
+        return self._pop_mean
+
+    @property
+    def pop_size(self):
+        """
+        The size of the full population.
+        """
+        return self._pop_size
+
+    @property
+    def oos_count(self):
+        """
+        The number of samples remaining in the population which haven't been sampled.
+        """
+        return self.pop_size - self.count
+
+    @property
+    def hist_oos_means(self):
+        """
+        The historical out-of-sample means of the data stream, up to the current time.
+        """
+        return np.array(self._oos_means)
+
+    @property
+    def prev_oos_mean(self):
+        """
+        The previous out-of-sample mean of the data stream, i.e. at the second-to-last
+        time point.
+        """
+        return self._oos_means[-2] if len(self._oos_means) > 1 else np.nan
+
+    @property
+    def oos_mean(self):
+        """
+        The current out-of-sample mean of the data stream.
+        """
+        return self._oos_means[-1] if self._oos_means else np.nan
+
+    @property
+    def hist_oos_sums(self):
+        """
+        The historical out-of-sample sums of the data stream, up to the current time.
+        """
+        return np.array(self._oos_sums)
+
+    @property
+    def prev_oos_sum(self):
+        """
+        The previous out-of-sample sum of the data stream, i.e. at the second-to-last
+        time point.
+        """
+        return self._oos_sums[-2] if len(self._oos_sums) > 1 else np.nan
+
+    @property
+    def oos_sum(self):
+        """
+        The current out-of-sample sum of the data stream.
+        """
+        return self._oos_sums[-1]
+
+    def add(self, x: np.ndarray) -> None:
+        if not isinstance(x, np.ndarray):
+            x = np.array(x, ndmin=1)
+        if x.size > self.oos_count:
+            raise TooManySamplesError(len(x), self.count, self.pop_size)
+        super().add(x)
+        new_oos_sums = [self._oos_sums[0] - s for s in self._sums[-len(x):]]
+        self._oos_sums.extend(new_oos_sums)
+        with np.errstate(divide="ignore", invalid="ignore"):
+            self._oos_means.extend(
+                new_oos_sums
+                / np.arange(self.oos_count + len(x) - 1, self.oos_count - 1, -1)
             )
