12f0cd3c6ad785357e16c2754882a23ab130f8b9
Floyd Everest <floyd.everest@monash.edu>
2024-06-26 06:22:23 +1000

    tweaked pyproject to add examples and ignore tests/examples where appropriate. Tweaked example

--- examples/alpha_estimators.py
+++ examples/alpha_estimators.py
@@ -2,7 +2,7 @@ import numpy as np
 import seaborn as sns
 import polars as pl
 
-from test_smart.nnm import AlphaMart, FixedBet, AGRAPA
+from test_smart.nnm import AlphaMart, FixedBet, AGRAPA, ShrinkTrunc
 
 
 n_total = 1000
@@ -10,27 +10,36 @@ n_sample = 500
 
 x = np.random.uniform(0.1, 1, n_total)
 
-alpha_st = AlphaMart(n_total=n_total)
+alpha_st0 = AlphaMart(n_total=n_total)
+alpha_st1 = AlphaMart(n_total=n_total, estim=ShrinkTrunc(eta0=.7))
 alpha_fb = AlphaMart(n_total=n_total, estim=FixedBet())
 alpha_agrapa = AlphaMart(n_total=n_total, estim=AGRAPA())
 
-alpha_st.update(x[:n_sample])
+alpha_st0.update(x[:n_sample])
+alpha_st1.update(x[:n_sample])
 alpha_fb.update(x[:n_sample])
 alpha_agrapa.update(x[:n_sample])
 
 df = pl.DataFrame(
     {
-        "index": np.tile(np.arange(1, n_sample + 1), 3),
+        "index": np.tile(np.arange(1, n_sample + 1), 4),
         "e-process": np.array(
-            alpha_agrapa.e_process + alpha_fb.e_process + alpha_st.e_process
+            alpha_agrapa.e_process
+            + alpha_fb.e_process
+            + alpha_st0.e_process
+            + alpha_st1.e_process
         ),
         "p-value": np.array(
-            alpha_agrapa.p_history + alpha_fb.p_history + alpha_st.p_history
+            alpha_agrapa.p_history
+            + alpha_fb.p_history
+            + alpha_st0.p_history
+            + alpha_st1.p_history
         ),
         "estimator": (
             ["aGRAPA"] * n_sample
             + ["Fixed Bet"] * n_sample
-            + ["Shrink Trunc"] * n_sample
+            + ["Shrink Trunc default"] * n_sample
+            + ["Shrink Trunc eta0=.7"] * n_sample
         ),
     }
 )
