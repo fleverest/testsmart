ae33f07c1435e467ab11f0a8d58b3d46d7cae9e1
Floyd Everest <floyd.everest@monash.edu>
2024-06-26 13:38:38 +1000

    Add a comparison of eta estimates for different models

--- examples/alpha_estimators.py
+++ examples/alpha_estimators.py
@@ -2,6 +2,8 @@ import numpy as np
 import seaborn as sns
 import polars as pl
 
+import matplotlib.pyplot as plt
+
 from test_smart.nnm import AlphaMart, FixedBet, AGRAPA, ShrinkTrunc
 
 
@@ -11,7 +13,7 @@ n_sample = 500
 x = np.random.uniform(0.1, 1, n_total)
 
 alpha_st0 = AlphaMart(n_total=n_total)
-alpha_st1 = AlphaMart(n_total=n_total, estim=ShrinkTrunc(eta0=.7))
+alpha_st1 = AlphaMart(n_total=n_total, estim=ShrinkTrunc(eta0=0.7))
 alpha_fb = AlphaMart(n_total=n_total, estim=FixedBet())
 alpha_agrapa = AlphaMart(n_total=n_total, estim=AGRAPA())
 
@@ -23,6 +25,7 @@ alpha_agrapa.update(x[:n_sample])
 df = pl.DataFrame(
     {
         "index": np.tile(np.arange(1, n_sample + 1), 4),
+        "sample mean": np.tile(alpha_st0.summaries.hist_means, 4),
         "e-process": np.array(
             alpha_agrapa.e_process
             + alpha_fb.e_process
@@ -35,6 +38,12 @@ df = pl.DataFrame(
             + alpha_st0.p_history
             + alpha_st1.p_history
         ),
+        "eta": np.array(
+            alpha_agrapa._eta_history
+            + alpha_fb._eta_history
+            + alpha_st0._eta_history
+            + alpha_st1._eta_history
+        ),
         "estimator": (
             ["aGRAPA"] * n_sample
             + ["Fixed Bet"] * n_sample
@@ -44,13 +53,20 @@ df = pl.DataFrame(
     }
 )
 
-plot1 = sns.lineplot(data=df, x="index", y="e-process", hue="estimator")
-plot1.axhline(20, ls="--")
-plot1.set_yscale("log")
-plot1.get_figure().savefig("compare_estim_eproc.png")
-plot1.clear()
+plt.figure(figsize=(10, 6))
+sns.lineplot(data=df, x="index", y="e-process", hue="estimator")
+plt.axhline(20, ls="--")
+plt.yscale("log")
+plt.savefig("compare_estim_eproc.png")
 
-plot2 = sns.lineplot(data=df, x="index", y="p-value", hue="estimator")
-plot2.axhline(1 / 20, ls="--")
-plot2.set_yscale("log")
-plot2.get_figure().savefig("compare_estim_pval.png")
+plt.figure(figsize=(10, 6))
+sns.lineplot(data=df, x="index", y="p-value", hue="estimator")
+plt.axhline(1 / 20, ls="--")
+plt.yscale("log")
+plt.savefig("compare_estim_pval.png")
+
+plt.figure(figsize=(10, 6))
+sns.lineplot(data=df, x="index", y="eta", hue="estimator")
+sns.lineplot(data=df, x="index", y="sample mean", label="sample mean")
+plt.axhline(0.5, ls="--")
+plt.savefig("compare_estim_eta.png")
